99200fe710cee96cdd901e01f24b7b35e6858359
malte0811 <malte0811@web.de>
2022-06-30 17:47:29 +0200

    Speed up builds by simplifying webpack config This needs some more testing, but seems to work fine for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
 const path = require('path');
 
 module.exports = {
-	entry: './js/render/entry_browser.ts',
+	entry: './js/render/entry_browser.js',
 	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				include: [
-					path.resolve(__dirname, "js/render"),
-					path.resolve(__dirname, "js/common"),
-				],
-				exclude: /node_modules/,
-			},
-		],
-	},
 	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
+		extensions: ['.js'],
 	},
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+    performance: {
+        // We do not care about this size; TT is never loaded over anything worse than a fast LAN connection
+        maxEntrypointSize: 5120000,
+        maxAssetSize: 5120000,
+    }
 };
