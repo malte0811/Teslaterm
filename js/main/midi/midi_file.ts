2604a63a4c7e485ee88c169f78ae66f1fa81fca4
malte0811 <malte0811@web.de>
2024-05-11 10:56:44 +0200

    Detect and warn if MIDI files have inconsistent mixer data

--- js/main/midi/midi_file.ts
+++ js/main/midi/midi_file.ts
@@ -1,6 +1,7 @@
 import {TransmittedFile} from "../../common/IPCConstantsToMain";
-import {ChannelID} from "../../common/IPCConstantsToRenderer";
+import {ChannelID, ToastSeverity} from "../../common/IPCConstantsToRenderer";
 import {MediaFileType} from "../../common/MediaTypes";
+import {isMulticoil} from "../connection/connection";
 import {ipcs} from "../ipc/IPCProvider";
 import {media_state} from "../media/media_player";
 import {player, startCurrentMidiFile, stopMidiFile} from "./midi";
@@ -14,37 +15,67 @@ function fixBrokenArray<T>(reallyTwoDimArray: T[]): T[] {
     return result;
 }
 
-export async function loadMidiFile(file: TransmittedFile) {
-    player.loadArrayBuffer(file.contents);
-    const events = fixBrokenArray(player.getEvents());
-    const uniqueChannels: number[] = [];
-    const programByChannel = new Map<ChannelID, number>();
-    const volumeByChannel = new Map<ChannelID, number>();
-    const multiProgramChannels = new Set<ChannelID>();
-    const nameByTrack = new Map<number, string>();
-    const trackByChannel = new Map<ChannelID, number>();
-    for (const event of events) {
-        if (event.name === 'Sequence/Track Name') {
-            nameByTrack.set(event.track, event.string);
-        }
-        if (event.channel !== undefined && !uniqueChannels.includes(event.channel)) {
-            uniqueChannels.push(event.channel);
-        }
-        // TODO detect invalid data
-        if (event.channel !== undefined && event.track !== undefined) {
-            trackByChannel.set(event.channel, event.track);
-        }
-        if (event.name === 'Program Change' && !multiProgramChannels.has(event.channel)) {
-            if (programByChannel.has(event.channel) && programByChannel.get(event.channel) !== event.value) {
-                console.warn(`Channel ${event.channel} has multiple program change events!`);
-                multiProgramChannels.add(event.channel);
-                programByChannel.delete(event.channel);
-            }
-            programByChannel.set(event.channel, event.value);
-        } else if (event.name === 'Controller Change' && event.number === 7) {
-            volumeByChannel.set(event.channel, event.value * (100 / 127));
+function addValue<K, T>(map: Map<K, T[]>, key: K, value: T) {
+    if (!map.has(key)) {
+        map.set(key, []);
+    }
+    const values = map.get(key);
+    if (!values.includes(value)) {
+        values.push(value);
+    }
+}
+
+function warnAndCleanMultivalues<K, T>(map: Map<K, T[]>, message: (key: K, count: number) => string) {
+    const result = new Map<K, T>();
+    let anyBad = false;
+    for (const [key, value] of map) {
+        if (value.length === 1) {
+            result.set(key, value[0]);
+        } else {
+            console.warn(message(key, value.length));
+            anyBad = true;
         }
     }
+    if (anyBad && isMulticoil()) {
+        ipcs.misc.openGenericToast(
+            'MIDI error',
+            'Inconsistent MIDI data found, mixer may not work as expected',
+            ToastSeverity.warning,
+            'inconsistent-midi',
+        );
+    }
+    return result;
+}
+
+export async function loadMidiFile(file: TransmittedFile) {
+    player.loadArrayBuffer(file.contents);
+    const uniqueChannels: number[] = [];
+    const programsByChannel = new Map<ChannelID, number[]>();
+    const volumesByChannel = new Map<ChannelID, number[]>();
+    const namesByTrack = new Map<number, string[]>();
+    const tracksByChannel = new Map<ChannelID, number[]>();
+    for (const event of fixBrokenArray(player.getEvents())) {
+        if (event.name === 'Sequence/Track Name') {
+            addValue(namesByTrack, event.track, event.string);
+        }
+        if (event.channel !== undefined) {
+            if (!uniqueChannels.includes(event.channel)) {
+                uniqueChannels.push(event.channel);
+            }
+            if (event.track !== undefined) {
+                addValue(tracksByChannel, event.channel, event.track);
+            }
+            if (event.name === 'Program Change') {
+                addValue(programsByChannel, event.channel, event.value);
+            } else if (event.name === 'Controller Change' && event.number === 7) {
+                addValue(volumesByChannel, event.channel, event.value * (100 / 127));
+            }
+        }
+    }
+    const nameByTrack = warnAndCleanMultivalues(namesByTrack, (t, n) => `Have ${n} names for track ${t}`);
+    const trackByChannel = warnAndCleanMultivalues(tracksByChannel, (k, n) => `${n} tracks access channel ${k}`);
+    const programByChannel = warnAndCleanMultivalues(programsByChannel, (k, n) => `Channel ${k} has ${n} programs`);
+    const volumeByChannel = warnAndCleanMultivalues(volumesByChannel, (k, n) => `Channel ${k} has ${n} preset volumes`);
     const nameByChannel = new Map<ChannelID, string>();
     for (const [channel, track] of trackByChannel) {
         nameByChannel.set(channel, nameByTrack.get(track) || `Channel ${channel}`);
