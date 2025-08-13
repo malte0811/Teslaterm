c0b5a0c58823de30e03e76c49bd112035c0ccf15
malte0811 <malte0811@web.de>
2025-08-13 19:55:51 +0200

    temp

--- js/render/vms/MapSelector.tsx
+++ js/render/vms/MapSelector.tsx
@@ -1,3 +1,6 @@
+import {useState} from "react";
+import {Button, ButtonGroup} from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 import {FullVMSData, MapReference, Program} from "../../common/VMS";
 
 interface MapSelectorPropsBase<T> {
@@ -10,28 +13,36 @@ interface MapSelectorPropsBase<T> {
 type MapSelectorProps = MapSelectorPropsBase<FullVMSData>;
 
 function SubMapSelector(props: MapSelectorPropsBase<Program> & {ownId: number}) {
-    const fullClassName = (type: string, selected: boolean) => {
-        return `vms-mapsel-${type}-row vms-mapsel-row-${selected ? 'selected' : 'default'}`;
-    };
     const current = props.currentSelection;
-    const mapDivs = props.data.maps.map((map, i) => {
-        return <div
+    const [expanded, setExpanded] = useState(false);
+    const mapDivs = expanded ? props.data.maps.map((map, i) => {
+        return <Button
             key={i}
-            className={fullClassName('map', props.ownId === current?.programId && i === current?.mapId)}
+            variant={props.ownId === current?.programId && i === current?.mapId ? 'danger' : 'info'}
             onClick={() => props.setSelection({programId: props.ownId, mapId: i})}
         >
             {map.startNote} - {map.endNote}
-        </div>;
-    });
-    return <div>
-        <div
-            className={fullClassName('program', props.ownId === current?.programId)}
-            onClick={() => props.setSelection({programId: props.ownId, mapId: 0})}
-        >
-            {props.data.name}
-        </div>
+        </Button>;
+    }) : [];
+    const onClick = () => {
+        if (!expanded) {
+            props.setSelection({programId: props.ownId, mapId: 0});
+        }
+        setExpanded(!expanded);
+    };
+    const variant = props.ownId === current?.programId ? 'danger' : 'info';
+    return <ButtonGroup vertical style={{width: '100%', paddingBottom: '10px'}}>
+        <Dropdown as={ButtonGroup}>
+            <Button variant={variant} onClick={onClick}>{props.data.name}</Button>
+            <Dropdown.Toggle split variant={variant} style={{flexGrow: 0}}/>
+            <Dropdown.Menu>
+                <Dropdown.Item>Action</Dropdown.Item>
+                <Dropdown.Item>Another action</Dropdown.Item>
+                <Dropdown.Item>Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
         {...mapDivs}
-    </div>;
+    </ButtonGroup>;
 }
 
 export function MapSelector(props: MapSelectorProps) {
@@ -41,6 +52,7 @@ export function MapSelector(props: MapSelectorProps) {
             currentSelection={props.currentSelection}
             setSelection={props.setSelection}
             ownId={i}
+            key={i}
         />)}
     </>;
 }
